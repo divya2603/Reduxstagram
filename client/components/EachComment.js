d4f5292f4c3eac755bd0f935fee46fe7f4c8f752
Divya Bharti <dbhart@sapient.com>
2017-04-09 22:49:08 +0530

    changes

--- client/components/EachComment.js
+++ client/components/EachComment.js
@@ -1,16 +1,8 @@
 import React from 'react';
 
 class EachComment extends React.Component {
-    constructor(props){
-        super(props);
-        // var result = this.props.postComments.map(function(comment) {return comment.text;});
-        // this.state={disabled:true,value:result}
-    }
-  // handleClick(e){
-  //   this.setState({disable:!this.state.disable,value:""})
-  // }
-
-    render() {
+ 
+     render() {
       var eachComment = this.props.postComments.map((comment,i) => 
         <div className="comment" key={i}>
           <p>
